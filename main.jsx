108c1a90a62891bb47bcc0dffdddbd0e4301bd2d
Kdogg85 <kylemallery85@gmail.com>
2025-05-05 09:50:15 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,7 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => <div style={{ textAlign: "center", padding: "2rem" }}><h1>Mew-Too</h1><p>The two-headed cult meme coin cat!</p></div>;
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
